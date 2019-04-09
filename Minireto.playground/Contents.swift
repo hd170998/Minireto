57e6668b5c5a649a3847ce3d72f608d9f6ed5f0a
Hector David Hernandez Rodriguez <hd170998@hotmail.com>
2019-04-08 22:31:54 -0500

    Corrección

--- Minireto.playground/Contents.swift
+++ Minireto.playground/Contents.swift
@@ -5,9 +5,9 @@ for i in 0...100 {
     }
     if i%2 != 0{
         print(i,"impar")
-        if i%5==0{
+    if i%5==0{
         print(i,"BINGO")
-        }
+    }
     }
     if (i>=30 && i<=40){
         print(i,"VIVA SWIFT")
